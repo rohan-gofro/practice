5573902fdad5e4a15885aca05baf77c8178b7bb7
rohan-gofro <rohan.gupta@gofro.com>
2017-07-06 10:33:43 +0530

    q3 updated

--- src/ques3/EmployeeDataHash.java
+++ src/ques3/EmployeeDataHash.java
@@ -53,11 +53,24 @@ public class EmployeeDataHash {
 			}
 			
 		}
+	}
+	
+	void showData()
+	{
+		
+		if(set.size() == 0)
+			System.out.println("No records to display");
+		
+		else
+			for(Employee i:set){
+				System.out.println(i.getEmpId() + " " + i.getEmpName() + " " + i.getEmpSalary());
+			}
 		
 	}
 			
 		
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
 		EmployeeDataHash obj1 = new EmployeeDataHash();
 		int choice = 0;
@@ -73,7 +86,8 @@ public class EmployeeDataHash {
 		    System.out.println("Enter Choice: ");
 		    choice = Integer.parseInt(sc.next()); 
 		    
-		    switch (choice) {
+		    switch (choice) 
+		    {
 			case 1:
 				System.out.println("Enter ID ");
 				empId = Long.parseLong(sc.next());
@@ -88,22 +102,19 @@ public class EmployeeDataHash {
 				break;
 			case 2:
 				empId = Long.parseLong(sc.next());
-				if(obj1.removeEmployee(new Employee(empId))){
-					
+				if(obj1.removeEmployee(new Employee(empId)))
 					System.out.println("Success");
-				}
-				else{
-					
+				
+				else					
 					System.out.println("Failed. Id not found");
-					
-				}
+				
 				break;
+			
 			case 3:
 				System.out.println("Enter Name: ");
 				empName = sc.next();
-				if(!obj1.searchWithName(empName)){
+				if(!obj1.searchWithName(empName))
 					System.out.println("No employee found with this name");
-				}
 				break;
 			
 			case 4:
@@ -112,12 +123,15 @@ public class EmployeeDataHash {
 				obj1.searchInSalary(empSal , empSal2);
 				break;
 
-			}
+		    
+		    case 5:
+		    	obj1.showData();		    	
+		    	break;
 		
 		}
-		while(choice!=5);
+		}
+		while(choice!=6);		
 		sc.close();
-		
 		System.out.println("Goodbye");
-	}
+}
 }
